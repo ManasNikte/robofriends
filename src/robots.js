f973dde3863c43b9066527f4027dbe251ee3323d
Manas Nikte <manas.nikte@gmail.com>
2022-04-24 20:36:33 +0530

    adding files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Vinnie The Pooh',
+    username: 'Vinayak',
+    email: 'vinu.bhau@mister.fresh'
+  },
+  {
+    id: 2,
+    name: 'The Gud Simp',
+    username: 'Kedar',
+    email: 'Kodremon@the.gud.simp'
+  },
+  {
+    id: 3,
+    name: 'Radhe Radhe',
+    username: 'Vivek',
+    email: 'radhe.radhe@easy.angry'
+  },
+  {
+    id: 4,
+    name: 'Goti Shot Guy',
+    username: 'Nikhil',
+    email: 'goti.shot@jockya.noob'
+  },
+  {
+    id: 5,
+    name: 'Mareli Avaj',
+    username: 'Swayam',
+    email: 'meri.avaj@aisi.hi.hai'
+  },
+  {
+    id: 6,
+    name: 'Oli Chaddi',
+    username: 'Rahul',
+    email: 'ek.chota@ek.bada'
+  },
+  {
+    id: 7,
+    name: 'Fast-Ass',
+    username: 'Tejas',
+    email: 'tejax@single.singer'
+  },
+  {
+    id: 8,
+    name: 'Sasta Kabir Singh',
+    username: 'Rajdeep',
+    email: 'saste.nashe@padhai.op'
+  },
+  {
+    id: 9,
+    name: 'The Happiness Dude',
+    username: 'Vishal',
+    email: 'no.khushi@in.life'
+  },
+  {
+    id: 10,
+    name: 'Manos',
+    username: 'Manas',
+    email: 'manos.nashedi@hutiyapa.com'
+  }
+];
